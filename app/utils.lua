d75880672498f2157cdf5b11b7e3b8057d749599
Ildar Akhmetgaleev <akhilman@gmail.com>
2018-11-20 03:39:30 +0000

    Reimplement partial without fun.chain.

--- app/utils.lua
+++ app/utils.lua
@@ -17,10 +17,25 @@ function M.merge_tables(...)
   return ret
 end
 
+function M.concatenate(...)
+
+  local ret = {}
+  local sources = {...}
+
+  for _, src in ipairs(sources) do
+    for _, v in ipairs(src) do
+      table.insert(ret, v)
+    end
+  end
+
+  return ret
+end
+
 function M.partial(func, ...)
-  local prefix_args = rx.util.pack(...)
+  local pre_args = rx.util.pack(...)
   return function(...)
-    local args = fun.totable(fun.chain(prefix_args, {...}))
+    local new_args = rx.util.pack(...)
+    local args = M.concatenate(pre_args, new_args)
     return func(rx.util.unpack(args))
   end
 end
